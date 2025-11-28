03e9f632c2d9e36f814bc01a41f0a84e3e1a7fe0
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-11-28 15:50:53 +0530

    Update environment variables and URLs in config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shoaib910385/SonyMusic",
+    "https://github.com/shoaib910385/valrikmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -122,6 +122,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
